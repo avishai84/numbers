3abef7eac24a26dceee7a717ac76c2241e712790
Avishai Taashur <avishai@avishai84.com>
2020-06-15 22:35:03 -0700

    footer info

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,6 +7,7 @@ const Footer:langFN = () => {
     <>
       <footer className="footer">
   Created By Avishai, Isabel & Sienna 2020 {dateYear <= 2020 ? '' : ' - ' + dateYear}
+      <small><a href="https://github.com/avishai84/numbers" target="_blank">Repo</a></small>
       </footer>
     </>
   );
